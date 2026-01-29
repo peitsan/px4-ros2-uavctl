414e2dde0a00b42d2b3f07f564e7ba52a4c7dbb4
paxon <paxon.lam@gmail.com>
2026-01-29 23:43:55 +0800

    upload-0129

--- src/px4_hexctl/include/px4_hexctl/offboard_control.hpp
+++ src/px4_hexctl/include/px4_hexctl/offboard_control.hpp
@@ -49,6 +49,14 @@ public:
     bool simulated_land(double descent_rate, double ground_tolerance, double timeout);
     bool fly_to_trajectory_setpoint(double x, double y, double z, double yaw, double timeout);
     
+    void reset_home() {
+        std::lock_guard<std::mutex> guard(lock_);
+        if (vehicle_local_position_received_) {
+            home_position_ = {vehicle_local_position_enu_.x, vehicle_local_position_enu_.y, vehicle_local_position_enu_.z};
+            RCLCPP_INFO(this->get_logger(), "🏠 [HOME] Reset to current: (%f, %f, %f)", home_position_[0], home_position_[1], home_position_[2]);
+        }
+    }
+
     bool takeoff_command_global(double altitude_m,
                                 double pitch_deg = 15.0,
                                 double yaw_deg = NAN,
